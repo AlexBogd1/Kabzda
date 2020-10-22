affac5cd111f71f95703e05626e004cb953b4817
Alex <7894664@mail.ru>
2020-10-22 16:41:08 +0300

    add onKeyPress function to select

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, KeyboardEvent} from "react";
 import style from './Select.module.css'
 
 type SelectType = {
@@ -12,11 +12,37 @@ type SelectType = {
 export function Select(props: SelectType) {
     const [hoveredElement, setHoveredElement] = useState(props.activeValue)
 
+
+    const onKeyUp = (e: KeyboardEvent<HTMLDivElement>) => {
+
+        for (let i = 0; i < props.values.length; i++) {
+            if (e.key === "ArrowDown" || e.key === "ArrowUp") {
+                const pretendElement = e.key === "ArrowDown" ? props.values[i + 1] : props.values[i - 1]
+                if (props.values[i] === hoveredElement) {
+                    props.setActiveValue(pretendElement);
+                    setHoveredElement(pretendElement);
+                }
+                if (props.values[i] === hoveredElement && props.values[i] === props.values[0] && e.key === "ArrowUp") {
+                    props.setActiveValue(props.values[props.values.length - 1]);
+                    setHoveredElement(props.values[props.values.length - 1]);
+                }
+                if (props.values[i] === hoveredElement && props.values[i] === props.values[props.values.length -1] && e.key === "ArrowDown") {
+                    props.setActiveValue(props.values[0]);
+                    setHoveredElement(props.values[0]);
+                }
+            }
+            if(e.key === "Enter" || e.key === "Escape"){
+                props.collapseSelect();
+            }
+
+        }
+    }
+
     return (
         <div>
 
 
-            <div className={style.wrapper}>
+            <div className={style.wrapper} onKeyUp={onKeyUp} tabIndex={0}>
                 <div
                     onClick={() => props.collapseSelect()}>
                     <b>{props.activeValue}</b>
